e86111fc68761f11ae3622051939d8c8622a45fd
Lucas Ambesi <ambesilucas@gmail.com>
2021-11-03 01:01:23 -0300

    creacion base proyecto

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/public'))
+
+// app.use("/public", express.static(__dirname + "/public"));
+
+app.listen(port, () => {
+  console.log("Servidor corriendo!");
+});
