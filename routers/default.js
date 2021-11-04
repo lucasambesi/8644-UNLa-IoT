a5646989874cb4dbc350776fc52cbe9644f5a4d3
Lucas Ambesi <ambesilucas@gmail.com>
2021-11-04 01:56:25 -0300

    import broker in default router

--- routers/default.js
+++ routers/default.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const conexion = require('../database/db')
+const broker = require('../brokerMqtt/mqtt')
 
 router.get('/', (req, res) => {
     // res.render('index', {
     //     tituloHead:'EJS',
     //     titulo : 'subtitulo dinamico'
     // })
+
     conexion.query('select * from dispositivos', (error, results) => {
       if (error) throw error
       else{
