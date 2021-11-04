423b002fe78d7a0008617ffe24cdc4801f4c3f67
Lucas Ambesi <ambesilucas@gmail.com>
2021-11-03 22:29:15 -0300

    se agrega insert de topicos a base de datos

--- app.js
+++ app.js
@@ -11,11 +11,6 @@ var con = mysql.createConnection({
 con.connect(function (err){
   if (err) throw err
   console.log("Conexion a MySQL OK")
-  let query = 'SELECT * FROM `dispositivos`'
-  con.query(query,function(err,result,fields){
-    if(err) throw err
-    else console.log(result);
-  })
 })
 
 var options = {
@@ -38,4 +33,10 @@ client.on('connect', () => {
 
 client.on('message',function(topic,message){
   console.log("Topico: "+ topic + " / Mensaje: " + message.toString())
+  
+  let query = "INSERT INTO `data` (`topic`, `mensaje`, `create_at`) VALUES (" + topic + "," +message.toString() + ", CURRENT_TIMESTAMP(6).000000)"
+  con.query(query,function(err,result,fields){
+    if(err) throw err
+    else console.log("Insert exitoso: " + result);
+  })
 })
